9fb6c6ec0234439d5610e0e574530104f2b1a356
s-nes347 <kokuanes@yahoo.co.jp>
2021-10-30 12:49:12 +0900

    template css js

--- comic_app/urls.py
+++ comic_app/urls.py
@@ -3,12 +3,14 @@ from . import views
 
 app_name = "comic_app"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name ="index"),
-    path("/title/<int:pk>/", views.TitleDetailView.as_view(), name="title_detail" ),
-    path("/register/author/", views.RegisterAuthorView.as_view(), name="registerauthor"),
-    path("/register/category/", views.RegisterCategoryView.as_view(), name="registercategory"),
-    path("/register/title/", views.RegisterTitleView.as_view(), name="registertitle"),
-    path("/register/evaluation/", views.RegisterEvaluationView.as_view(), name="registerevaluation"),
-    path("/update/evaluation/<int:pk>/", views.UpdateEvaluationView.as_view(), name='update/evaluation'),
+    #path("", views.IndexView.as_view(), name ="index"),
+    path("title/<int:pk>/", views.TitleDetailView.as_view(), name="title_detail" ),
+    path("register/author/", views.RegisterAuthorView.as_view(), name="registerauthor"),
+    path("register/category/", views.RegisterCategoryView.as_view(), name="registercategory"),
+    path("register/title/", views.RegisterTitleView.as_view(), name="registertitle"),
+    path("register/evaluation/", views.RegisterEvaluationView.as_view(), name="registerevaluation"),
+    path("update/evaluation/<int:pk>/", views.UpdateEvaluationView.as_view(), name='update/evaluation'),
+    path("",views.NewIndexView.as_view(), name="index"),
+    path("sub/",views.SubPageView.as_view(), name="subpage"),
     
 ]
--- comic_app/views.py
+++ comic_app/views.py
@@ -4,12 +4,13 @@ from django.urls import reverse
 from comic_app.form import AuthorForm, CategoryForm, TitleForm, EvaluationForm
 from comic_app.models import Author, Title, Category , Evaluation
 
+"""
 class IndexView(generic.ListView):
     template_name = "comic_app/index.html"
     context_objects_name = "title_list"
     queryset = Title.objects.all()
 
-"""
+
 Function-viewの場合
 
 def index(request):
@@ -81,3 +82,15 @@ class UpdateEvaluationView(generic.UpdateView):
     template_name = "comic_app/register.html"
     def get_success_url(self):
         return reverse("comic_app:index")
+
+
+class NewIndexView(generic.ListView):
+    template_name = "comic_app/newindex.html"
+    context_objects_name = "title_list"
+    queryset = Title.objects.order_by('watch_date').reverse()[:3]
+
+
+class SubPageView(generic.ListView):
+    model = Title
+    template_name = "comic_app/subpage.html"
+    
